27b010df01f258be8511964b9618f2f816e50494
RushaidKhan <rushaidk7@gmail.com>
2024-02-07 21:12:58 +0530

    minor changes

--- app/src/main/java/com/rush/weatherapp2/MainActivity.kt
+++ app/src/main/java/com/rush/weatherapp2/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        fetchWeatherData("london")
+        fetchWeatherData("Colombo")
         SearchCity()
     }
 
